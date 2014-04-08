1d21b5bdef277490d71dc69b19182d9c8fbb6d89
giles-t <giles.tamplin@gmail.com>
2014-04-08 10:50:52 -0700

    Linted

--- dev/js/routers/router.js
+++ dev/js/routers/router.js
@@ -3,49 +3,47 @@
 
 // Includes file dependencies
 define([ "jquery", "backbone", "auth", "debug",
-	// View Manager
-	"../framework/viewManager",
-	// Views
-	"../views/entry/SignInView",
-	"../views/entry/SignUpView",
-	"../views/explore/ExploreView",
-	"../views/profile/ProfileView",
-	"../views/performer/PerformerView" 
+  // View Manager
+  "../framework/viewManager",
+  // Views
+  "../views/entry/SignInView",
+  "../views/entry/SignUpView",
+  "../views/explore/ExploreView",
+  "../views/profile/ProfileView",
+  "../views/performer/PerformerView" 
 ], function( $, Backbone, Auth, Debug,
-	// View Manager
-	viewManager,
-	// Views
-	EntrySignInView,
-	EntrySignUpView,
-	ExploreView,
-	ProfileView,
-	PerformerView
+  // View Manager
+  viewManager,
+  // Views
+  EntrySignInView,
+  EntrySignUpView,
+  ExploreView,
+  ProfileView,
+  PerformerView
 ) {
 
     var Router = Backbone.Router.extend( {
 
-        // The Router constructor
-        initialize: function() {
-			Debug.log('Router - Initialize!!!!');
-			
-			// Start listening for events
+      // The Router constructor
+      initialize: function() {
+        Debug.log('Router - Initialize!!!!');
+      
+      // Start listening for events
             Backbone.history.start();
         },
 
         // App Routes
         routes: {
-
-            "": "splash",
-			"signIn": "signIn",
-			"signUp": "signUp",
-			"tour": "tour",
-			"stream": "stream",
-            "explore": "explore",
-			"add": "add",
-			"activity": "activity",
-			"profile": "profile",
-			"performer/:performer": "performer"
-
+          "": "splash",
+          "signIn": "signIn",
+          "signUp": "signUp",
+          "tour": "tour",
+          "stream": "stream",
+          "explore": "explore",
+          "add": "add",
+          "activity": "activity",
+          "profile": "profile",
+          "performer/:performer": "performer"
         },
 
         // Entry Methods
@@ -53,51 +51,51 @@ define([ "jquery", "backbone", "auth", "debug",
             $.mobile.changePage( "#signUpView" , { reverse: false, changeHash: false } );
         },
         signIn: function() {
-			Debug.log('Router - signIn route!!!!')
-			this.navClear("topNav");
-			viewManager.create('#signInView', EntrySignInView, false, false, false);
+          Debug.log('Router - signIn route!!!!')
+          this.navClear("topNav");
+          viewManager.create('#signInView', EntrySignInView, false, false, false);
         },
         signUp: function() {
-			Debug.log('Router - signUp route!!!!')
-			this.navClear("topNav");
-			viewManager.create('#signUpView', EntrySignUpView, false, false, false);
+          Debug.log('Router - signUp route!!!!')
+          this.navClear("topNav");
+          viewManager.create('#signUpView', EntrySignUpView, false, false, false);
         },
         tour: function() {
-			this.navClear("topNav");
-            $.mobile.changePage( "#tourView" , { reverse: false, changeHash: false } );
+          this.navClear("topNav");
+          $.mobile.changePage( "#tourView" , { reverse: false, changeHash: false } );
         },
 
         // Main Page Methods
         stream: function() {
-			this.navClear('bottomNav');
-            $.mobile.changePage( "#streamView" , { reverse: false, changeHash: false } );
+          this.navClear('bottomNav');
+          $.mobile.changePage( "#streamView" , { reverse: false, changeHash: false } );
         },
         explore: function() {
-			this.navClear('bottomNav');
-			viewManager.create('#exploreView', ExploreView, true, false, false);
+          this.navClear('bottomNav');
+          viewManager.create('#exploreView', ExploreView, true, false, false);
         },
         add: function() {
-			this.navClear('bottomNav');
-            $.mobile.changePage( "#addView" , { reverse: false, changeHash: false } );
+          this.navClear('bottomNav');
+          $.mobile.changePage( "#addView" , { reverse: false, changeHash: false } );
         },
         activity: function() {
-			this.navClear('bottomNav');
-            $.mobile.changePage( "#activityView" , { reverse: false, changeHash: false } );
+          this.navClear('bottomNav');
+          $.mobile.changePage( "#activityView" , { reverse: false, changeHash: false } );
         },
         profile: function() {
-			this.navClear('bottomNav');
-			viewManager.create('#profileView', ProfileView, false, false, false);
+          this.navClear('bottomNav');
+          viewManager.create('#profileView', ProfileView, false, false, false);
         },
 
         // Dynamic Pages
         performer: function(performer) {
-			viewManager.create('#performerView', PerformerView, true, false, false);
+          viewManager.create('#performerView', PerformerView, true, false, false);
         },
 
-		// This clears active nav button state
-		navClear: function(nav) {
-			$('.' + nav + '').find('.ui-btn-active').removeClass('ui-btn-active');
-		},
+    // This clears active nav button state
+    navClear: function(nav) {
+      $('.' + nav + '').find('.ui-btn-active').removeClass('ui-btn-active');
+    },
 
     });
 
